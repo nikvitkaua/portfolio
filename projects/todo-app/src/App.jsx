51b5f08f46b1b322bc0478aebeac9822bd347f6e
Mykyta Paziuka <nikvitka.it@gmail.com>
2025-02-16 04:59:48 +0200

    add TaskInput to proj

--- projects/todo-app/src/App.jsx
+++ projects/todo-app/src/App.jsx
@@ -1,8 +1,24 @@
+import {useState} from "react";
+
+import TaskInput from "./components/TaskInput";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (taskText) => {
+    setTasks([...tasks, { id: Date.now(), text: taskText, completed: false }]);
+  }
+
   return (
     <div>
       <h1>Todo List</h1>
+      <TaskInput addTask={addTask} />
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
