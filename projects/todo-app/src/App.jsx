28bb5aaa4f7a12fd5dcecc3f74bf7102aedd953d
Mykyta Paziuka <nikvitka.it@gmail.com>
2025-02-16 15:58:48 +0200

    add tailwind and styles to proj

--- projects/todo-app/src/App.jsx
+++ projects/todo-app/src/App.jsx
@@ -43,16 +43,36 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>Todo List</h1>
+    <div className="max-w-full mx-auto bg-white shadow-md rounded-lg p-6 mt-10">
+      <h1 className="text-2xl font-bold text-gray-800 mb-4">Todo List</h1>
       <TaskInput addTask={addTask} />
 
-      <ul>
+      <ul className="mt-4">
         {tasks.map((task) => (
-          <li key={task.id} style={{ textDecoration: task.completed ? 'line-through' : '' }}>
-            <span>{task.createdAt}</span> | {task.text}
-            <button onClick={() => toggleCompleted(task.id)}>{task.completed ? 'Todo' : 'Completed'}</button>
-            <button onClick={(e) => { e.stopPropagation(); deleteTask(task.id); }}>Delete</button>
+          <li 
+            key={task.id} 
+            className={`p-3 mb-2 flex justify-between items-center border rounded-lg shadow-md transition
+              ${task.completed ? "bg-gray-100 text-gray-500" : "bg-white"}
+            `}
+          >
+            <div className={`${task.completed ? "line-through" : ""}`}>
+              <span className="text-amber-900 mr-5">{task.createdAt}</span>
+              <span className="max-w-full text-gray-800">{task.text}</span>
+            </div>
+            <div className="flex gap-2">
+              <button 
+                onClick={() => toggleCompleted(task.id)}
+                 className="text-green-500 hover:text-green-600 cursor-pointer"
+              >
+                {task.completed ? '❌' : '✔️'}
+              </button>
+              <button 
+                onClick={(e) => { e.stopPropagation(); deleteTask(task.id); }}
+                className="text-red-500 hover:text-red-600 cursor-pointer"
+              >
+                🗑️
+              </button>
+            </div>
           </li>
         ))}
       </ul>
